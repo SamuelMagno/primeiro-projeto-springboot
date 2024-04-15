90a76b0e459b1fe658da66cfd6796ba0bb3b5d64
Samuel Magno <samuelrtfmagno@gmail.com>
2024-04-15 19:00:17 -0300

    query params

--- src/main/java/br/com/samuelmagno/primeiroprojetospringboot/controller/PrimeiraController.java
+++ src/main/java/br/com/samuelmagno/primeiroprojetospringboot/controller/PrimeiraController.java
@@ -2,14 +2,31 @@ package br.com.samuelmagno.primeiroprojetospringboot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
 @RequestMapping("/primeiracontroller")
 public class PrimeiraController {
-    @GetMapping("/primeirometodo")
-    public String primeiroMetodo() {
-        return "Primeira API Rest com Spring";
+    @GetMapping("/primeirometodo/{id}")
+    public String primeiroMetodo(@PathVariable String id) {
+        return "Primeira API Rest com Spring. Param ID: " + id;
     }
+
+    @GetMapping("/metodoQueryParam")
+    public String metodoQueryParam(@RequestParam String id) {
+        return "Param é " + id;
+    }
+
+    @GetMapping("/metodoQueryParamAll")
+    public String metodoQueryParamAll(@RequestParam Map<String, String> allParams) {
+        return "Param é " + allParams.entrySet();
+    }
+    
 }
