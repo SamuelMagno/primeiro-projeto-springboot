48c9f9e2cbc8c8e30aacc12cf114f87f826fb7f5
Samuel Magno <samuelrtfmagno@gmail.com>
2024-04-15 19:27:07 -0300

    reponse entity e headers

--- src/main/java/br/com/samuelmagno/primeiroprojetospringboot/controller/PrimeiraController.java
+++ src/main/java/br/com/samuelmagno/primeiroprojetospringboot/controller/PrimeiraController.java
@@ -7,11 +7,15 @@ import java.util.Map;
 
 import javax.swing.text.html.parser.Entity;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 
 
@@ -40,6 +44,30 @@ public class PrimeiraController {
     }
     
     record Usuario(String username) {
+    }
+
+    @PostMapping("metodoComHeaders")
+    public String metodoComHeaders(@RequestHeader("name") String name) {
+        return "Metodo com header: " + name;
+    }
+
+    @PostMapping("metodoComHeadersAll")
+    public String metodoComHeadersAll(@RequestHeader Map<String, String> headers) {
+        return "Metodo com header: " + headers.entrySet();
+    }
+
+    @GetMapping("/metodoResponseEntity")
+    public ResponseEntity<Object> metodoResponseEntity() {
+        return ResponseEntity.status(HttpStatus.OK).body("Mensagem");
+    }
+
+    @GetMapping("/metodoResponseEntityTwo/{id}")
+    public ResponseEntity<Object> metodoResponseEntityTwo(@PathVariable Long id) {
+        if (id > 5) {
+            return ResponseEntity.status(HttpStatus.OK).body("Mensagem: Id " + id);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Mensagem Error");
+        }
     }
     
 }
